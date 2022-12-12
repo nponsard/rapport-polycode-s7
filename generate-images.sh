3953d546b57a7de3b3c148519ae7665aa69f840f
Nils Ponsard <nilsponsard@gmail.com>
2022-12-12 16:58:20 +0100

    fix: ci

--- generate-images.sh
+++ generate-images.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
+# test if DRAWIO is set
+if [ -z "$DRAWIO" ]; then
+  DRAWIO=drawio
+fi
+
+
 files=$(ls -d sketches/*/*.drawio)
 
 for file in $files
 do
   echo "Processing $file"
-  drawio -x -f png $file
+  $DRAWIO -x -f png $file
 done
